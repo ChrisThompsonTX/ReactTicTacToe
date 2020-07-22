ea2597b6047c18b654bb2fa2cca8e7d32cc1477d
Chris Thompson <chris.tx86@gmail.com>
2020-07-22 12:09:14 -0700

    pass click function to squares

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -4,15 +4,12 @@ import './index.css';
 
 class Square extends React.Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        };
-    }
     render() {
         return (
-            <button className="square" onClick={() => {alert('click'); }}>
+            <button 
+                className="square" 
+                onClick={ () => this.props.onClick() }
+            >
                 {this.props.value}
             </button>
         );
@@ -20,8 +17,19 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            squares: Array(9).fill(null),
+        }
+    }
     renderSquare(i) {
-        return <Square value={i} />;
+        return (
+            <Square 
+                value={this.state.squares[i]}
+                onClick={() => this.handleClick(i)}
+            />);
     }
 
     render() {
